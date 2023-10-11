f1bb78a08079ace22a20a7b4fb6ca10003d555b2
Andrii Morozov <mad060605@gmail.com>
2023-10-11 16:41:10 +0300

    create, display and delete - done

--- Propaganda/js/script.js
+++ Propaganda/js/script.js
@@ -1,46 +1,82 @@
-const searchInput = document.getElementById('input');
-const cards = document.querySelectorAll('.card');
-const countButton = document.getElementById('counter__button');
-const counterTotal = document.getElementById('counter__total');
-const sortButton = document.getElementById('sort__button');
+document.addEventListener("DOMContentLoaded", function() {
+  const searchInput = document.getElementById('input');
+  const counterTotal = document.getElementById('counter__total');
+  const countButton = document.getElementById('counter__button'); 
+  const sortButton = document.getElementById('sort__button');
+  const deleteButton = document.getElementById('delete__button');
+  const createdCards = document.getElementById('created__cards');
 
-searchInput.addEventListener('input', () => {
-  const searchText = searchInput.value.toLowerCase();
+  let cards = JSON.parse(localStorage.getItem('cards')) || [];
 
   cards.forEach(card => {
-    const cardTitle = card.querySelector('.card__title').textContent.toLowerCase();
-    const cardPropagandaAmount = card.querySelector('.card__propaganda__amount').textContent;
+    const cardElem = document.createElement('div');
+    const cardImg = document.createElement('img');
+    const cardTitle = document.createElement('p');
+    const cardPropagandaAmount = document.createElement('var');
 
-    if (cardTitle.includes(searchText) || cardPropagandaAmount.includes(searchText)) {
-      card.style.display = 'block';
-    } else {
-      card.style.display = 'none';
-    }
+    cardElem.className = 'card';
+    cardImg.src = '../Propaganda/img/mao.jpg';
+    cardImg.alt = 'mao';
+    cardImg.style.width = '200px';
+    cardTitle.className = 'card__title';
+    cardTitle.innerHTML = `<p>${card.cardTitle}</p>`;
+    cardPropagandaAmount.className = 'card__propaganda__amount';
+    cardPropagandaAmount.innerHTML = `<var>${card.cardPropagandaAmount}</var>`
+
+    cardElem.appendChild(cardImg);
+    cardElem.appendChild(cardTitle);
+    cardElem.appendChild(cardPropagandaAmount);
+
+    createdCards.appendChild(cardElem);
+  });
+
+  searchInput.addEventListener('input', () => {
+    const searchText = searchInput.value.toLowerCase();
+
+    const cards = createdCards.querySelectorAll('.card');
+
+    cards.forEach(card => {
+      const cardTitle = card.querySelector('.card__title').textContent.toLowerCase();
+      const cardPropagandaAmount = card.querySelector('.card__propaganda__amount').textContent;
+
+      if (cardTitle.includes(searchText) || cardPropagandaAmount.includes(searchText)) {
+        card.style.display = 'block';
+      } else {
+        card.style.display = 'none';
+      }
+    });
+  });
+
+  countButton.addEventListener('click', () => {
+    let totalPropagandaAmount = 0;
+
+    const visibleCards = createdCards.querySelectorAll('.card:not([style*="display: none"])');
+
+    visibleCards.forEach(card => {
+      const cardPropagandaAmount = card.querySelector('.card__propaganda__amount').textContent;
+      totalPropagandaAmount += parseInt(cardPropagandaAmount);
+    });
+
+    counterTotal.textContent = `Total amount: ${totalPropagandaAmount}`
+  });
+
+  sortButton.addEventListener('click', () => {
+    const cardList = Array.from(createdCards.querySelectorAll('.card:not([style*="display: none"])'));
+
+    cardList.sort((a, b) => {
+      const cardPropagandaAmountFirst = parseInt(a.querySelector('.card__propaganda__amount').textContent);
+      const cardPropagandaAmountSecond = parseInt(b.querySelector('.card__propaganda__amount').textContent);
+
+      return cardPropagandaAmountSecond - cardPropagandaAmountFirst;
+    });
+
+    cardList.forEach(card => {
+      createdCards.appendChild(card);
+    });
+  });
+
+  deleteButton.addEventListener('click', () => {
+    cards = [];
+    localStorage.setItem('cards', JSON.stringify([]));
   });
 });
-
-countButton.addEventListener('click', () =>{
-    let totalPropagandaAmount = 0;
-    
-    cards.forEach(card =>{
-        if(card.style.display !== 'none'){
-            const cardPropagandaAmount = card.querySelector('.card__propaganda__amount').textContent;
-
-            totalPropagandaAmount += parseInt(cardPropagandaAmount);
-        }  
-    });
-    counterTotal.textContent = `Total amount: ${totalPropagandaAmount}`
-});
-
-sortButton.addEventListener('click', () => {
-    Array.from(cards).sort((a,b) =>{
-        const cardPropagandaAmountFirst = parseInt(a.querySelector('.card__propaganda__amount').textContent);
-        const cardPropagandaAmountSecond = parseInt(b.querySelector('.card__propaganda__amount').textContent);
-
-        return cardPropagandaAmountSecond - cardPropagandaAmountFirst;
-    })
-    .forEach(card => {
-        card.parentNode.appendChild(card);
-      });
-});
-// :TODO expression function
